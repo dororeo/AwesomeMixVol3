562422aecb877b2c430c3ed0daa0b4eba9461076
Dorothea Lim Hui Ying <Dorothea@Internationals-MacBook-Pro.local>
2023-06-15 23:02:47 +0800

    fixed repeat button and all functions are complete

--- app.js
+++ app.js
@@ -11,8 +11,7 @@ const slider = document.querySelector(".slider");
 
 let currentTime = document.querySelector(".current-time");
 let songDuration = document.querySelector(".song-duration");
-let shuffleIcon = document.querySelector('.fa-shuffle');
-
+let shuffleIcon = document.querySelector(".fa-shuffle");
 
 let currentSong = document.createElement("audio");
 let songIndex = 0;
@@ -22,198 +21,192 @@ let repeatSong = false;
 let updateTime;
 
 const trackList = [
-    {
-        name : "Creep (Acoustic)",
-        artist : "Radiohead",
-        music : "songs/01.mp3"
-    },
-    {
-        name : "Crazy on You",
-        artist : "Heart",
-        music : "songs/02.mp3"
-    },
-    {
-        name : "Since You've Been Gone",
-        artist : "Rainbow",
-        music : "songs/03.mp3"
-    },
-    {
-        name : "In the Meantime",
-        artist : "Spacehog",
-        music : "songs/04.mp3"
-    },
-    {
-        name : "I'm Always Chasing Rainbows",
-        artist : "Alice Cooper",
-        music : "songs/05.mp3"
-    },
-    {
-        name : "San Francisco",
-        artist : "The Mowgli's",
-        music : "songs/06.mp3"
-    },
-    {
-        name : "No Sleep Till Brooklyn",
-        artist : "Beastie Boys",
-        music : "songs/07.mp3"
-    },
-    {
-        name : "Dog Days Are Over",
-        artist : "Florence + The Machine",
-        music : "songs/08.mp3"
-    }
+  {
+    name: "Creep (Acoustic)",
+    artist: "Radiohead",
+    music: "songs/01.mp3",
+  },
+  {
+    name: "Crazy on You",
+    artist: "Heart",
+    music: "songs/02.mp3",
+  },
+  {
+    name: "Since You've Been Gone",
+    artist: "Rainbow",
+    music: "songs/03.mp3",
+  },
+  {
+    name: "In the Meantime",
+    artist: "Spacehog",
+    music: "songs/04.mp3",
+  },
+  {
+    name: "I'm Always Chasing Rainbows",
+    artist: "Alice Cooper",
+    music: "songs/05.mp3",
+  },
+  {
+    name: "San Francisco",
+    artist: "The Mowgli's",
+    music: "songs/06.mp3",
+  },
+  {
+    name: "No Sleep Till Brooklyn",
+    artist: "Beastie Boys",
+    music: "songs/07.mp3",
+  },
+  {
+    name: "Dog Days Are Over",
+    artist: "Florence + The Machine",
+    music: "songs/08.mp3",
+  },
 ];
-
 
 //1. load audio, song title, artist name
 const loadSong = (songIndex) => {
-    clearInterval(updateTime);
-    reset();
+  clearInterval(updateTime);
+  reset();
 
-    currentSong.src = trackList[songIndex].music;
-    currentSong.load();
+  currentSong.src = trackList[songIndex].music;
+  currentSong.load();
 
-    songTitle.innerText = trackList[songIndex].name;
-    artistName.innerText = trackList[songIndex].artist;
+  songTitle.innerText = trackList[songIndex].name;
+  artistName.innerText = trackList[songIndex].artist;
 
-    updateTime = setInterval(setUpdate, 1000);
-    currentSong.addEventListener("ended", nextSong);
-
+  updateTime = setInterval(setUpdate, 1000);
+  currentSong.addEventListener("ended", nextSong);
 };
 
-//2. reset after song ends 
+//2. reset after song ends
 const reset = () => {
-    currentTime.innerText = "00:00"
-    songDuration.innerText = "00:00"
-    slider.value = 0;
+  currentTime.innerText = "00:00";
+  songDuration.innerText = "00:00";
+  slider.value = 0;
 };
-/////////////////////////////////////////////////////
 
 //overall shuffle function
 const shuffle = () => {
-    randomSong ? shuffleOff() : shuffleOn();
-    shuffleIcon.classList.add('randomActive');
+  randomSong ? shuffleOff() : shuffleOn();
+  shuffleIcon.classList.add("randomActive");
 };
 
 //5. random song
 const shuffleOn = () => {
-    randomSong = true;
+  randomSong = true;
 };
 
 const shuffleOff = () => {
-    randomSong = false;
+  randomSong = false;
 };
-
-
 
 //overall repeat function
-
 const repeating = () => {
-    repeatSong ? repeatOff() : repeatOn();
+  repeatSong ? repeatOff() : repeatOn();
 };
 
-//8. repeat song
+// 8. Repeat song
 const repeatOn = () => {
-    repeatSong = true;
-    currentSong.loop();
+  repeatSong = true;
+  currentSong.loop = true;
+  // repeatBtn.classList.add("active");
 };
 
 const repeatOff = () => {
-    repeatSong = false;
-    nextSong();
+  repeatSong = false;
+  currentSong.loop = false;
+  // repeatBtn.classList.remove("active");
 };
 
-/////////////////////////////////////////////////////
-
 //overall playPause function
-
 const playPause = () => {
-    songPlaying ? pauseSong() : playSong();
+  songPlaying ? pauseSong() : playSong();
 };
 
 //3. play song
 const playSong = () => {
-    currentSong.play();
-    songPlaying = true;
-    playPauseBtn.innerHTML = '<i class="fa-sharp fa-solid fa-pause"></i>';
+  currentSong.play();
+  songPlaying = true;
+  playPauseBtn.innerHTML = '<i class="fa-sharp fa-solid fa-pause"></i>';
 
-    //////rotate tape rings of cassette/////
-    // rotateRings();
+  //////rotate tape rings of cassette/////
+  // rotateRings();
 };
 
 //4. pause song
 const pauseSong = () => {
-    currentSong.pause();
-    songPlaying = false;
-    playPauseBtn.innerHTML = '<i class="fa-solid fa-play"></i>';
+  currentSong.pause();
+  songPlaying = false;
+  playPauseBtn.innerHTML = '<i class="fa-solid fa-play"></i>';
 };
+
 //6. next song
 /* if loop to check for shuffle, 
 add 1 to index number for next song else math.random */
 
 const nextSong = () => {
-    //if the current song index is less than the last song in the tracklist, and shuffleOff
-    if(songIndex < trackList.length - 1 && randomSong === false) {
-        songIndex += 1;
-    } else if( songIndex < trackList.length -1 && randomSong === true) {
-        //make it an integer because the length will be returned as a string//
-        let randomIndex = Number.parseInt(Math.random() * trackList.length);
-        songIndex = randomIndex;
-    } else {
-        songIndex = 0;
-    }
-    loadSong(songIndex);
-    playSong();
+  //if the current song index is less than the last song in the tracklist, and shuffleOff
+  if (songIndex < trackList.length - 1 && randomSong === false) {
+    songIndex += 1;
+  } else if (songIndex < trackList.length - 1 && randomSong === true) {
+    //make it an integer because the length will be returned as a string//
+    let randomIndex = Number.parseInt(Math.random() * trackList.length);
+    songIndex = randomIndex;
+  } else {
+    songIndex = 0;
+  }
+  loadSong(songIndex);
+  playSong();
 };
 
 //7. prev song
 const prevSong = () => {
-    if(songIndex > 0) {
-        songIndex -= 1;
-    } else {
-        songIndex = trackList.length -1;
-    }
-    loadSong(songIndex);
-    playSong();
+  if (songIndex > 0) {
+    songIndex -= 1;
+  } else {
+    songIndex = trackList.length - 1;
+  }
+  loadSong(songIndex);
+  playSong();
 };
 
 //9. slider update when clicked/dragged
-  
+
 const slideTo = () => {
-    //calculate position by percentage of slider relative to duration of the song
-    let slideTo = currentSong.duration * (slider.value / 100);
-    currentSong.currentTime = slideTo;
+  //calculate position by percentage of slider relative to duration of the song
+  let slideTo = currentSong.duration * (slider.value / 100);
+  currentSong.currentTime = slideTo;
 };
 
 //setUpdate function
 
 const setUpdate = () => {
-    let seekPosition = 0;
-    if (!isNaN(currentSong.duration)) {
-        seekPosition = (currentSong.currentTime / currentSong.duration) * 100;
-        slider.value = seekPosition;
-  
-      let currentMins = Math.floor(currentSong.currentTime / 60);
-      let currentSecs = Math.floor(currentSong.currentTime - currentMins * 60);
-      let durationMins = Math.floor(currentSong.duration / 60);
-      let durationSecs = Math.floor(currentSong.duration - durationMins * 60);
-  
-      if (currentSecs < 10) {
-        currentSecs = "0" + currentSecs;
-      }
-      if (durationSecs < 10) {
-        durationSecs = "0" + durationSecs;
-      }
-      if (currentMins < 10) {
-        currentMins = "0" + currentMins;
-      }
-      if (durationMins < 10) {
-        durationMins = "0" + durationMins;
-      }
-  
-      currentTime.innerHTML = currentMins + ":" + currentSecs;
-      songDuration.innerHTML = durationMins + ":" + durationSecs;
+  let seekPosition = 0;
+  if (!isNaN(currentSong.duration)) {
+    seekPosition = (currentSong.currentTime / currentSong.duration) * 100;
+    slider.value = seekPosition;
+
+    let currentMins = Math.floor(currentSong.currentTime / 60);
+    let currentSecs = Math.floor(currentSong.currentTime - currentMins * 60);
+    let durationMins = Math.floor(currentSong.duration / 60);
+    let durationSecs = Math.floor(currentSong.duration - durationMins * 60);
+
+    if (currentSecs < 10) {
+      currentSecs = "0" + currentSecs;
     }
-  };
+    if (durationSecs < 10) {
+      durationSecs = "0" + durationSecs;
+    }
+    if (currentMins < 10) {
+      currentMins = "0" + currentMins;
+    }
+    if (durationMins < 10) {
+      durationMins = "0" + durationMins;
+    }
+
+    currentTime.innerHTML = currentMins + ":" + currentSecs;
+    songDuration.innerHTML = durationMins + ":" + durationSecs;
+  }
+};
 
 loadSong(songIndex);
